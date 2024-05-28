0332b8fad3c0632ec16cc2a0e38a5c62a1ddbc20
Katenka-pu6 <146732153+Katenka-pu6@users.noreply.github.com>
2024-05-28 11:01:01 +0300

    mdl

---
+++ model/Teacher.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.util.Random;
+
+@SuppressWarnings("unused")
+public class Teacher extends User {
+    private int teacherId;
+
+
+    public Teacher(String firstName, String lastName, String middleName, int teacherId) {
+        super(firstName, lastName, middleName);
+    }
+
+    public int getTeacherId() {
+        return teacherId;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher: " +
+                "Name: " + super.getFirstName() +
+                ", Last name: " + super.getLastName() +
+                ", Middle name: " + super.getMiddleName() +
+                ", Teacher ID: " + teacherId;
+    }
+}
